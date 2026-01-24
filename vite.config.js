a47358aaf7489ad39a245adcd62e5ef2fb8a9b5a
cuongphamduc <cuongphamduc1997@gmail.com>
2026-01-24 19:06:51 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/aireborn/',
+  server: {
+    allowedHosts: ['aispeech.ptit.edu.vn'],
+  },
 })
