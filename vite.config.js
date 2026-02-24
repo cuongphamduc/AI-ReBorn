0f214914d43bf83fca6568119c0b585ffefe212b
cuongphamduc <cuongphamduc1997@gmail.com>
2026-02-24 18:55:06 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/aireborn/',   // Đường dẫn gốc khi deploy (ứng dụng nằm tại domain/aireborn/)
   server: {
+    host: '0.0.0.0',
     allowedHosts: ['aispeech.ptit.edu.vn'],  // Cho phép truy cập từ domain này
   },
 })
